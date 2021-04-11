a139f83f6c956e9d20c4f92a668b8f4fd2d85ddc
ShinJaehun <gunsmoke.shin@gmail.com>
2021-04-11 19:12:30 +0900

    Edit reply and channel views

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,18 +1,23 @@
 class DiscussionsController < ApplicationController
   before_action :set_discussion, only: %i[ show edit update destroy ]
+  before_action :find_channels, only: [:index, :show, :new, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /discussions or /discussions.json
   def index
-    @discussions = Discussion.all
+    # @discussions = Discussion.all
+    @discussions = Discussion.all.order('created_at desc')
   end
 
   # GET /discussions/1 or /discussions/1.json
   def show
+    @discussions = Discussion.all.order('created_at desc')
   end
 
   # GET /discussions/new
   def new
-    @discussion = Discussion.new
+    # @discussion = Discussion.new
+    @discussion = current_user.discussions.build
   end
 
   # GET /discussions/1/edit
@@ -21,7 +26,8 @@ class DiscussionsController < ApplicationController
 
   # POST /discussions or /discussions.json
   def create
-    @discussion = Discussion.new(discussion_params)
+    # @discussion = Discussion.new(discussion_params)
+    @discussion = current_user.discussions.build(discussion_params)
 
     respond_to do |format|
       if @discussion.save
@@ -62,8 +68,12 @@ class DiscussionsController < ApplicationController
       @discussion = Discussion.find(params[:id])
     end
 
+    def find_channels
+      @channels = Channel.all.order('created_at desc')
+    end
+
     # Only allow a list of trusted parameters through.
     def discussion_params
-      params.require(:discussion).permit(:title, :content)
+      params.require(:discussion).permit(:title, :content, :channel_id)
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+
+#  def strip_markdown(text)
+#    markdown_to_plain_text = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
+#    markdown_to_plain_text.render(text).html_safe
+#  end
 end
