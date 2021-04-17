6b83d4d68690309ef3d537bbdb2de4e321616f22
ShinJaehun <gunsmoke.shin@gmail.com>
2021-04-17 15:18:39 +0900

    jquery/jquery-ujs 설정 및 friendly_id gem 적용

--- Gemfile
+++ Gemfile
@@ -51,6 +51,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'guard-livereload'
+  gem 'rails_db'
 end
 
 group :test do
--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -6,4 +6,10 @@ class Discussion < ApplicationRecord
 
   validates :title, :content, presence: true
   resourcify #이걸 왜 하는지는 모르겠음....
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :finders]
+
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 end
